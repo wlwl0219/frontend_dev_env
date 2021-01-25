477999b8d8840b09cf9a3b34d54fe9c72e8692ec
wlwl0219 <wlwl0219@gmail.com>
2021-01-26 03:49:44 +0900

    Add plugin

--- src/app.js
+++ src/app.js
@@ -2,3 +2,9 @@ import * as math from "./math.js";
 import "./style.css";
 
 console.log(math.sum(1, 2));
+console.log(process.env.NODE_ENV); // development
+console.log(TWO); // 2
+console.log(VERSION); // 'v.1.2.3'
+console.log(PRODUCTION); // false
+console.log(MAX_COUNT); // 999
+console.log(api.domain); // 'http://dev.api.domain.com'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const webpack = require("webpack");
+const banner = require("./banner.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -13,17 +18,51 @@ module.exports = {
     rules: [
       {
         test: /\.css$/, // .css 확장자로 끝나는 모든 파일
-        use: ["style-loader", "css-loader"], // css-loader를 적용한다
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
+          "css-loader",
+        ],
       },
       {
         test: /\.jpeg$/, // .png 확장자로 마치는 모든 파일
-        loader: "url-loader", // 파일 로더를 적용한다
+        loader: "file-loader", // 파일 로더를 적용한다
         options: {
-          publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
+          publicPath: "./", // prefix를 아웃풋 경로로 지정
           name: "[name].[ext]?[hash]", // 파일명 형식
-          limit: 5000, // 5kb 미만 파일만 data url로 처리
+          // limit: 5000, // 5kb 미만 파일만 data url로 처리
         },
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin(banner),
+    new webpack.DefinePlugin({
+      TWO: "1+1",
+      VERSION: JSON.stringify("v.1.2.3"),
+      PRODUCTION: JSON.stringify(false),
+      MAX_COUNT: JSON.stringify(999),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // 템플릿 경로를 지정
+      templateParameters: {
+        // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+      hash: true,
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : []),
+  ],
 };
