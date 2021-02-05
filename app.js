dc3c79838c9ea83e546660b2b995358a330f0759
wlwl0219 <wlwl0219@gmail.com>
2021-02-05 20:03:28 +0900

    Add lint

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const alert = (msg) => window.alert(msg);
-new Promise();
+console.log("hello world");
+
+console.log(
+  "----------------매 우 긴 문 장 입 니 다 80자가 넘 는 코 드 입 니 다.----------------"
+);
+
+foo(
+  reallyLongArg(),
+  omgSoManyParameters(),
+  IShouldRefactorThis(),
+  isThereSeriouslyAnotherOne()
+);
