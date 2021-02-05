9bcea8f1458b75a5ef569229a324915653acc3ca
wlwl0219 <wlwl0219@gmail.com>
2021-02-05 20:03:47 +0900

    Add lint

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ console.log(
   "----------------매 우 긴 문 장 입 니 다 80자가 넘 는 코 드 입 니 다.----------------"
 );
 
-foo(
-  reallyLongArg(),
-  omgSoManyParameters(),
-  IShouldRefactorThis(),
-  isThereSeriouslyAnotherOne()
-);
+// foo(
+//   reallyLongArg(),
+//   omgSoManyParameters(),
+//   IShouldRefactorThis(),
+//   isThereSeriouslyAnotherOne()
+// );
